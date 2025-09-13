5876a4ecbcb99a00b515780c07075993c83edebc
atfox272 <trananhtai577@gmail.com>
2025-09-13 20:10:33 +0700

    mod: change resizing size

--- tb/ImgRsz_tb.sv
+++ tb/ImgRsz_tb.sv
@@ -31,6 +31,17 @@ module ImgRsz_tb;
     FcRszPxlBuf_t                           FcRszPxlBuf; // Block accumulated value
     logic       [RSZ_IMG_HEIGHT_SIZE-1:0]   [RSZ_IMG_WIDTH_SIZE-1:0]  RszPxlParVld;  // Block is executed by Compute Engine
     
+    typedef struct packed {
+        logic   [15:0]  PosX;
+        logic   [15:0]  PosY;
+        logic   [7:0]   Data;
+    } PxlInfo_s;
+
+
+    PxlInfo_s           PxlInfoMem      [0:IMG_WIDTH_MAX_SIZE*IMG_HEIGHT_MAX_SIZE-1];
+    logic       [15:0]  ImgInfo         [0:1];
+    PxlInfo_s           RszPxlInfoMem   [0:RSZ_IMG_WIDTH_SIZE*RSZ_IMG_HEIGHT_SIZE-1];
+    
     ImgRsz dut (.*);
 
     initial begin
@@ -52,36 +63,31 @@ module ImgRsz_tb;
     initial begin
         forever #1 Clk <= ~Clk;
     end
-
+    
     
     initial begin
-        #100000; $finish; 
+        #1000000; 
+        $writememh("L:/Projects/ImageResizer/sim/env/RszPxlInfo.txt", RszPxlInfoMem);
+        $finish; 
     end
-    
+
+    initial begin
+        $readmemh("L:/Projects/ImageResizer/sim/env/PxlInfo.txt", PxlInfoMem);
+        $readmemh("L:/Projects/ImageResizer/sim/env/ImgInfo.txt", ImgInfo);
+    end
+
     initial begin
         logic [PXL_PRIM_COLOR_W-1:0] PxlDataTemp [PXL_PRIM_COLOR_NUM-1:0];
         int                          RandStall;
         #20;
         Cl;
-        for (int y=0; y<DT_IMG_HEIGHT; y++) begin
-            for (int x=0; x<DT_IMG_WIDTH; x++) begin
-                PxlDataTemp[0] = x;
-                PxlStDrv(.Data(PxlDataTemp), .X(x), .Y(y),
-                .Width(DT_IMG_WIDTH), .Height(DT_IMG_HEIGHT));
-            end
-            // Random bubbles
-            RandStall =  $urandom_range(PXL_ST_I_STALL_MIN, PXL_ST_I_STALL_MAX);
-            repeat(RandStall) Cl;
-        end
-
-        
-        #20;
-        Cl;
-        for (int y=0; y<DT_IMG_HEIGHT; y++) begin
-            for (int x=0; x<DT_IMG_WIDTH; x++) begin
-                PxlDataTemp[0] = x;
-                PxlStDrv(.Data(PxlDataTemp), .X(x), .Y(y),
-                .Width(DT_IMG_WIDTH), .Height(DT_IMG_HEIGHT));
+        for (int y=0; y<ImgInfo[1]; y++) begin
+            for (int x=0; x<ImgInfo[0]; x++) begin
+                PxlStDrv(.Data  (PxlInfoMem[y*ImgInfo[0] + x].Data),
+                         .X     (PxlInfoMem[y*ImgInfo[0] + x].PosX),
+                         .Y     (PxlInfoMem[y*ImgInfo[0] + x].PosY),
+                         .Width (ImgInfo[0]),
+                         .Height(ImgInfo[1]));
             end
             // Random bubbles
             RandStall =  $urandom_range(PXL_ST_I_STALL_MIN, PXL_ST_I_STALL_MAX);
@@ -96,29 +102,34 @@ module ImgRsz_tb;
         int                                     RandStall;
         #20;
         Cl;
-        while (1'b1) begin
-            RszPxlSamp( .SampRszPxlData (RszPxlDataTemp),
-                        .SampRszPxlX    (RszPxlXTemp),
-                        .SampRszPxlY    (RszPxlYTemp));
-            $display("[INFO]: Resized Pixel     |   Data-%d  |    X-%d   |   Y-%d    |", RszPxlDataTemp[0], RszPxlXTemp, RszPxlYTemp);
-            
-            // Random bubbles
-            RandStall =  $urandom_range(PXL_ST_O_STALL_MIN, PXL_ST_O_STALL_MAX);
-            repeat(RandStall) Cl;
+        for (int y=0; y<RSZ_IMG_HEIGHT_SIZE; y++) begin
+            for (int x=0; x<RSZ_IMG_WIDTH_SIZE; x++) begin
+                RszPxlSamp( .SampRszPxlData (RszPxlDataTemp),
+                            .SampRszPxlX    (RszPxlXTemp),
+                            .SampRszPxlY    (RszPxlYTemp));
+                $display("[INFO]: Resized Pixel     |   Data-%d  |    X-%d   |   Y-%d    |", RszPxlDataTemp[0], RszPxlXTemp, RszPxlYTemp);
+                // Store the resized pixel
+                RszPxlInfoMem[y*RSZ_IMG_WIDTH_SIZE + x].Data = RszPxlDataTemp[0];
+                RszPxlInfoMem[y*RSZ_IMG_WIDTH_SIZE + x].PosX = (16)'(RszPxlXTemp);
+                RszPxlInfoMem[y*RSZ_IMG_WIDTH_SIZE + x].PosY = (16)'(RszPxlYTemp);
+                // Random bubbles
+                RandStall =  $urandom_range(PXL_ST_O_STALL_MIN, PXL_ST_O_STALL_MAX);
+                repeat(RandStall) Cl;
+            end
         end
-        
+        // $writememh("L:/Projects/ImageResizer/sim/env/RszPxlInfo.txt", RszPxlInfoMem);
     end
 
     // Pixel Streaming Driver
     task PxlStDrv(
-        input [PXL_PRIM_COLOR_W-1:0]    Data     [PXL_PRIM_COLOR_NUM-1:0],
+        input [PXL_PRIM_COLOR_W-1:0]    Data,
         input [IMG_WIDTH_IDX_W-1:0]     X,
         input [IMG_HEIGHT_IDX_W-1:0]    Y,
         input [IMG_WIDTH_IDX_W-1:0]     Width,
         input [IMG_HEIGHT_IDX_W-1:0]    Height
     );
         for (int i=0; i<PXL_PRIM_COLOR_NUM; i++) begin
-            PxlData[i] = Data[i];
+            PxlData[i] = Data;
         end
         PxlX        = X;
         PxlY        = Y;
